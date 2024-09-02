f7b67e309a511d3a991ce6bfc36d86fcacd6b0bb
Naruhiro Ikeya <naruhiro.ikeya@beex-inc.com>
2024-09-02 17:56:00 +0900

    20240902

--- bin/RemoveObjectsS3.bat
+++ bin/RemoveObjectsS3.bat
@@ -30,7 +30,7 @@ SET __ERROR_CODE__=-1
 ::      パラメータチェック       ::
 :::::::::::::::::::::::::::::::::::
 SET __ARGC__=0
-FOR %%a IN ( %* ) DO SET /A __ARGC__+=1
+FOR /F "usebackq" %%L IN (`powershell -command "\"%*\".split(\" \").count"`) DO SET __ARGC__=%%L
 
 IF %__ARGC__% lss 4 (
   SET __TIME__=%TIME:~0,8%
